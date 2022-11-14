ac9c4eea12d2624ac063fe637f18ef60c587f358
RamTheeru <theertha808@gmail.com>
2022-11-14 08:56:12 +0530

    3 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -11,14 +11,44 @@ export class TestComponent implements OnInit {
   val: string = '';
   isset: boolean = false;
   nums: number[] = [1, 2, 3, 4, 5];
+  classT: string = '';
+  color: string = 'green';
   constructor() {}
+  public myclasses = {
+    'text-success': !this.isset,
+    'text-danger': this.isset,
+    'text-special': true,
+  };
+  public mystyles = {
+    color: 'red',
+    fontStyle: 'bold',
+  };
 
   ngOnInit() {
     //this.val = this.getdata;
     //this.isset = this.val != '';
   }
+  passval(val) {
+    console.log(val.tagName);
+    var tag = val.tagName;
+    if (tag == 'P') {
+      alert('Itsa paragraph');
+    }
+  }
   oncheck() {
+    this.color = 'blue';
+    this.isset = true;
     this.val = this.getdata;
+    this.myclasses = {
+      'text-success': !this.isset,
+      'text-danger': this.isset,
+      'text-special': true,
+    };
+    this.mystyles = {
+      color: 'blue',
+      fontStyle: 'italic',
+    };
+    this.classT = 'blue';
     //this.isset = this.ival != '';
   }
   passtoparent() {
