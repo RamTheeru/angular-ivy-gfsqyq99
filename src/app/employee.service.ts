17510db0ae4b596bf46c677a4b8d42a90d74a9e6
RamTheeru <theertha808@gmail.com>
2022-11-17 18:39:08 +0530

    1 file modified

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Emp } from './emp';
 import { Observable } from 'rxjs';
+import * as R from 'rxjs';
 
 @Injectable()
 export class EmployeeService {
   constructor(private http: HttpClient) {}
   url: string =
     // 'https://github.com/RamTheeru/angular-ivy-gfsqyq99/blob/main/src/app/assets/emp.json';
-    './assets/emp.json';
+    '/assets/emp.json';
   getemployees(): Observable<Emp[]> {
     return this.http.get<Emp[]>(this.url);
   }
